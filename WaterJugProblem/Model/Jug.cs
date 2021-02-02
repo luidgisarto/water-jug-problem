fc945b53df21bd1e1906f89e376208d1c659a66a
Julio Trindade <jtrindade@ice.ufjf.br>
2021-02-01 22:10:53 -0300

    Alterações nas classes

---
+++ WaterJugProblem/Model/Jug.cs
@@ -0,0 +1,50 @@
+﻿namespace WaterJugProblem.Model
+{
+    public class Jug
+    {
+        public Jug(int capacity)
+        {
+            Capacity = capacity;
+            Current = 0;
+        }
+
+        public int Capacity { get; private set; }
+
+        public int Current { get; private set; }
+
+        public int EmptySpace
+        {
+            get
+            {
+                return Capacity - Current;
+            }
+        }
+
+        public void UpdateContent(int amount)
+        {
+            Current += amount;
+        }
+
+        public bool IsEmpty()
+        {
+            return Current == 0;
+        }
+
+        public bool IsFull()
+        {
+            return Current == Capacity;
+        }
+
+        public bool FillJug()
+        {
+            Current = Capacity;
+            return true;
+        }
+
+        public bool EmptyJug()
+        {
+            Current = 0;
+            return true;
+        }
+    }
+}
---
+++ WaterJugProblem/Model/JugProblem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WaterJugProblem.Model
+{
+    public class JugProblem
+    {
+        readonly Dictionary<int, Func<bool>> _rules;
+
+        public JugProblem(int m, int n)
+        {
+            j1 = new Jug(m);
+            j2 = new Jug(n);
+
+            _rules = new Dictionary<int, Func<bool>>()
+            {
+                { 1, j1.FillJug },
+                { 2, j2.FillJug },
+                { 3, j1.EmptyJug },
+                { 4, j2.EmptyJug },
+                { 5, () => TransferWater(j1, j2) },
+                { 6, () => TransferWater(j2, j1) }
+            };
+        }
+
+        public Jug j1 { get; set; }
+
+        public Jug j2 { get; set; }
+
+        public bool TransferWater(Jug current, Jug destination)
+        {
+            if (current.IsEmpty() || destination.IsFull())
+                return false;
+
+            var amount = Math.Min(current.Current, destination.EmptySpace);
+
+            current.UpdateContent(-amount);
+            destination.UpdateContent(amount);
+
+            return true;
+        }
+    }
+}
